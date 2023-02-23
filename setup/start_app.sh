417424c05a02d300d1fe775284cf11e5d857f86f
Md Mosharaf Hossan <mosharafkuet@gmail.com>
2023-02-23 16:07:42 +0600

    Add bash file to start the app easily

---
+++ setup/start_app.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Create a copy of the example environment file
+cp .env.example .env
+
+# Install the project dependencies using Composer
+composer install
+
+# Start the Docker environment using Laravel Sail
+./vendor/bin/sail up -d
+
+# Generate a new application key
+./vendor/bin/sail artisan key:generate
+
+# Run the database migrations to set up the database schema
+./vendor/bin/sail artisan migrate
